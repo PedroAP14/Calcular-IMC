c3058cacaa06c2fde8a0c43ffa190ccb2a842587
PedroAP14 <pamericoparussolo@gmail.com>
2026-09-19 14:20:11 -0300

    v2

--- calcularimc/MainPage.xaml.cs
+++ calcularimc/MainPage.xaml.cs
@@ -12,71 +12,78 @@
             double imc;
             double altura = 0;
             double peso = 0;
+            double pesoAdequado;
 
-            if (NomeEntry.Text == null || IdadeEntry.Text == null || GeneroPicker.SelectedIndex == -1)
+            if (NomeEntry.Text == null || NomeEntry.Text == "" || IdadeEntry.Text == null || IdadeEntry.Text == "" || GeneroPicker.SelectedIndex == -1)
             {
-                AvisoLabel.Text = "Insira todas as informações!!!";
-                return;
+                ImcLabel.Text = ImcLabel.Text = $"IMC:"; ;
+                SugestaoPesoLabel.Text = null;
+
+                ClassificacaoPesoLabel.Text = "Insira todas as informações!!!";
+                ClassificacaoPesoLabel.TextColor = Microsoft.Maui.Graphics.Colors.Red;
             }
                 
             else
             {
-               altura = Convert.ToDouble(AlturaLabel.Text);
-               peso = Convert.ToDouble(PesoLabel.Text);
-
-                AvisoLabel.Text = null;
+                altura = Convert.ToDouble(AlturaLabel.Text);
+                peso = Convert.ToDouble(PesoLabel.Text);
 
                 imc = Math.Round(peso / (altura * altura), 1);
+                pesoAdequado = Math.Round(21.7 * (altura * altura), 1);
+
+                ImcLabel.Text = $"IMC: {imc.ToString()}";
 
                 if (imc < 18.5)
                 {
                     ClassificacaoPesoLabel.Text = "Abaixo do Peso";
-                    ClassificacaoPesoLabel.TextColor = Microsoft.Maui.Graphics.Colors.Red;
+                    ClassificacaoPesoLabel.TextColor = Microsoft.Maui.Graphics.Colors.DarkRed;
+                    SugestaoPesoLabel.Text = $"Peso Adequado: {pesoAdequado.ToString()}Kg";
                 }
                 else if (imc < 25)
                 {
                     ClassificacaoPesoLabel.Text = "Peso Adequado";
                     ClassificacaoPesoLabel.TextColor = Microsoft.Maui.Graphics.Colors.LightGreen;
+                    SugestaoPesoLabel.Text = null;
                 }
                 else if (imc < 30)
                 {
                     ClassificacaoPesoLabel.Text = "SobrePeso";
                     ClassificacaoPesoLabel.TextColor = Microsoft.Maui.Graphics.Colors.DarkGreen;
+                    SugestaoPesoLabel.Text = $"Peso Adequado: {pesoAdequado.ToString()}Kg";
                 }
                 else if (imc < 35)
                 {
                     ClassificacaoPesoLabel.Text = "Obesidade Grau 1";
                     ClassificacaoPesoLabel.TextColor = Microsoft.Maui.Graphics.Colors.Orange;
+                    SugestaoPesoLabel.Text = $"Peso Adequado: {pesoAdequado.ToString()}Kg";
                 }
                 else if (imc < 40)
                 {
                     ClassificacaoPesoLabel.Text = "Obesidade Grau 2";
                     ClassificacaoPesoLabel.TextColor = Microsoft.Maui.Graphics.Colors.OrangeRed;
+                    SugestaoPesoLabel.Text = $"Peso Adequado: {pesoAdequado.ToString()}Kg";
                 }
                 else
                 {
                     ClassificacaoPesoLabel.Text = "Obesidade Grau 3";
                     ClassificacaoPesoLabel.TextColor = Microsoft.Maui.Graphics.Colors.DarkRed;
+                    SugestaoPesoLabel.Text = $"Peso Adequado: {pesoAdequado.ToString()}Kg";
                 }
 
-
-                ImcLabel.Text = $" IMC: {imc.ToString()}";
             }
        
         }
 
         private void AlturaSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            double alturaArredondado = Math.Round(AlturaSlider.Value, 2);
-            string alturaTexto = alturaArredondado.ToString();
-            AlturaLabel.Text = $"{alturaTexto}";
+            double alturaSlider = Math.Round(AlturaSlider.Value, 2);
+            AlturaLabel.Text = $"{alturaSlider}";
         }
 
         private void PesoSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            double pesoArredondado = Math.Round(PesoSlider.Value, 2);
-            string pesoTexto = pesoArredondado.ToString();
-            PesoLabel.Text = $"{pesoTexto}";
+            double pesoSlider = Math.Round(PesoSlider.Value, 1);
+            PesoLabel.Text = $"{pesoSlider}";
         }
     }
 }
